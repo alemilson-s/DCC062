2c66c89b26286e87796404418b2aa6942f9653ac
Alemilson Silva <alemilson@ice.ufjf.br>
2023-07-15 19:18:30 -0300

    .

--- untitled/src/main/java/lbk/Process.java
+++ untitled/src/main/java/lbk/Process.java
@@ -1,2 +1,62 @@
-package lbk;public class Process {
+package lbk;
+
+//Class Process{
+//        (str)name, (int)priority, (int)id, (float)runtime, (int)size_memory;
+//}
+public class Process {
+    private static int n_id = 0;
+    private String name;
+    private int priority;
+    private int id;
+    private float runtime;
+    private int size_memory;
+
+    public Process(String name, int priority, float runtime, int size_memory) {
+        this.name = name;
+        this.priority = priority;
+        this.runtime = runtime;
+        this.size_memory = size_memory;
+        this.id = Process.n_id;
+        Process.n_id++;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getPriority() {
+        return priority;
+    }
+
+    public void setPriority(int priority) {
+        this.priority = priority;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public float getRuntime() {
+        return runtime;
+    }
+
+    public void setRuntime(float runtime) {
+        this.runtime = runtime;
+    }
+
+    public int getSize_memory() {
+        return size_memory;
+    }
+
+    public void setSize_memory(int size_memory) {
+        this.size_memory = size_memory;
+    }
 }
